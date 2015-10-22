e97a5a1d999a80fb6f9e7b12d914496001d9549f
michael-iden <michael.iden@mybuys.com>
2015-10-22 10:19:32 -0400

    Added sprint progress

---
+++ radiator/jobs/sprint_progress.rb
@@ -0,0 +1,26 @@
+require 'jira'
+
+jira_credentials = JSON.parse( IO.read("assets/credentials/shadow-jira-credentials.json"))
+
+JIRA_CONFIG = {
+    :username     => jira_credentials["username"],
+    :password     => jira_credentials["password"],
+    :site         => jira_credentials["site"],
+    :auth_type    => :basic,
+    :context_path => ''
+}
+
+SCHEDULER.every '5m', :first_in => 0 do |job|
+  client = JIRA::Client.new(JIRA_CONFIG)
+  closed_points = client.Issue.jql("sprint in openSprints()  and project = \"Web Rec UI\" and status = \"done\"").map{ |issue| issue.fields['customfield_10005'] }.reduce(:+) || 0
+  total_points = client.Issue.jql("sprint in openSprints()  and project = \"Web Rec UI\"").map{ |issue| issue.fields['customfield_10005'] }.reduce(:+) || 0
+  if total_points == 0
+    percentage = 0
+    moreinfo = "No sprint currently in progress"
+  else
+    percentage = ((closed_points/total_points)*100).to_i
+    moreinfo = "#{closed_points.to_i} / #{total_points.to_i}"
+  end
+
+  send_event('sprintProgress', { title: "Sprint Progress", min: 0, value: percentage, max: 100, moreinfo: moreinfo })
+end
